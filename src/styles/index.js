436a1f882ad18d417571d71aef3ec167578c132e
Ali <D32.Secret@yahoo.com>
2021-01-02 13:39:43 +0330

    Create styles for layout

--- src/styles/index.js
+++ src/styles/index.js
@@ -1,3 +1,6 @@
+import { makeStyles } from "@material-ui/core/styles";
+import { NAVBAR_WIDTH } from "../constants";
+
 export const useEmptyLayoutStyles = makeStyles((theme) => ({
   root: {
     height: "100vh",
@@ -9,5 +12,36 @@ export const useEmptyLayoutStyles = makeStyles((theme) => ({
     backgroundSize: "cover",
     backgroundPosition: "center",
     zIndex: -1,
+  },
+}));
+
+export const useLayoutStyles = makeStyles((theme) => ({
+  root: {
+    display: "flex",
+  },
+  toolbar: theme.mixins.toolbar, // necessary for content to be below top bar
+  content: {
+    width: "100%",
+    flexGrow: 1,
+    backgroundColor: theme.palette.background.dark,
+  },
+}));
+
+export const useTopBarStyles = makeStyles((theme) => ({
+  root: {
+    [theme.breakpoints.up("lg")]: {
+      width: `calc(100% - ${NAVBAR_WIDTH}px)`,
+      marginLeft: NAVBAR_WIDTH,
+    },
+  },
+}));
+
+export const useNavBarStyles = makeStyles(() => ({
+  root: {
+    width: NAVBAR_WIDTH,
+    flexShrink: 0,
+  },
+  paper: {
+    width: NAVBAR_WIDTH,
   },
 }));
